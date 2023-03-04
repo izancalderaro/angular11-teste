cbba9afd1050c9c3c818d77a980242353b7a321b
Izan Calderaro <ncti2006-2015@yahoo.com.br>
2023-03-04 14:08:39 -0500

    Revisão

--- src/app/modules/arquivo/arquivo.component.ts
+++ src/app/modules/arquivo/arquivo.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { ThemePalette } from '@angular/material/core';
-import * as fs from 'fs';
 
 @Component({
   selector: 'app-arquivo',
@@ -14,34 +13,9 @@ export class ArquivoComponent implements OnInit {
   color2: ThemePalette = 'accent';
   color3: ThemePalette = 'warn';
 
-  /**Delimitadores
-   *
-   * @params ¥
-   * Unicode number:U+00A5
-   * HTML-code: &#165;
-   *
-   * @params £
-   * Unicode number: U+00A3
-   * HTML-code: &#163;
-   *
-   * */
-
-  separador: string = '';
-  delimitador: string = '';
-  fim: string = 'FIM' + this.separador + this.delimitador;
-  //Tipo de registros
-  deletado: string = 'E';
-  header: string = 'H';
-  detail: string = 'D';
-  trailer: string = 'T';
-
   constructor() {}
 
-  ngOnInit(): void {
-    fs.writeFile('c:/sip.txt', this.fim, (erro) => {
-      if (erro) console.info('Erro gerado' + erro);
-    });
-  }
+  ngOnInit(): void {}
 
   lerArquivo() {}
 
--- src/app/modules/forms/form-data/form-data.component.ts
+++ src/app/modules/forms/form-data/form-data.component.ts
@@ -1,68 +1,61 @@
-
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
 import { shareReplay } from 'rxjs/operators';
 
-
-
 @Component({
   selector: 'app-form-data',
   templateUrl: './form-data.component.html',
-  styleUrls: ['./form-data.component.scss']
+  styleUrls: ['./form-data.component.scss'],
 })
 export class FormDataComponent implements OnInit {
-/**
- *
-*/
+  /**
+   *
+   */
   formData = new FormData();
-  apiUrl = 'https://gateway.stg.cloud.pje.jus.br/previdenciario-api';
-  endPoint = '/api/v1/intimacao-judicial/4.0.1/extrair';
+  apiUrl = '';
+  endPoint = '';
   header: HttpHeaders;
-  file: string = ''
+  file: string = '';
 
   constructor(private _http: HttpClient) {
-    this.header =  new HttpHeaders({
-      'Accept': '*/*',
-      'X-PDPJ-CPF-USUARIO-OPERADOR':'93758798412',
-      'Authorization': 'bearer ' + this.getToken(),
-      // 'Content-Type':'multipart/form-data, boundary=X-PREVIDENCIARIO',
-
-    })
+    /** */
+    this.header = new HttpHeaders({});
+    this.header.append('Accept', '*/*');
+    this.header.append('Authorization', 'bearer ' + this.getToken());
   }
 
-  ngOnInit(): void {/** */}
+  ngOnInit(): void {
+    /** */
+  }
 
-  carregaHtml(evento:any) {
+  carregaHtml(evento: any) {
     /** */
 
-   const file: File = evento.target.files[0]
+    const file: File = evento.target.files[0];
 
-    this.formData.append('file', file)
+    this.formData.append('file', file);
 
-    this.file = file.name
+    this.file = file.name;
   }
 
   enviaHtml() {
-
-    this.envioService().subscribe(
-      (result: any) => console.log(result)
-    )
- }
+    this.envioService().subscribe((result: any) => console.log(result));
+  }
 
   envioService(): Observable<any> {
     /** */
-    return this._http.post<any>(`${this.apiUrl}${this.endPoint}`,
-      this.formData,
-      { headers: this.header }).pipe(shareReplay())
-
+    return this._http
+      .post<any>(`${this.apiUrl}${this.endPoint}`, this.formData, {
+        headers: this.header,
+      })
+      .pipe(shareReplay());
   }
 
   getToken(): string {
     /** */
-    const token = 'eyJhbGciOiJSUzI1NiIsInR5cCIgOiAiSldUIiwia2lkIiA6ICI1dnJEZ1hCS21FLTdFb3J2a0U1TXU5VmxJZF9JU2dsMnY3QWYyM25EdkRVIn0.eyJleHAiOjE2NTc3ODI1MDAsImlhdCI6MTY1NzczOTMwMCwianRpIjoiOGMzNjE4MWYtMDI5MC00MjkxLTg2NjUtNTJhOGQ2ZDEzYzZkIiwiaXNzIjoiaHR0cHM6Ly9zc28uc3RnLmNsb3VkLnBqZS5qdXMuYnIvYXV0aC9yZWFsbXMvcGplIiwiYXVkIjpbImp1TVAtYmFja2VuZCIsImFjY291bnQiXSwic3ViIjoiYmRiMTE3YWMtNTQ0Ny00NjBkLTkwYWMtNTZlMjQyYmFjNGQ2IiwidHlwIjoiQmVhcmVyIiwiYXpwIjoibm90aWZpY2FjYW8iLCJzZXNzaW9uX3N0YXRlIjoiM2JiMTUwZDMtMDYyYi00ZmMwLWI2MTMtOTEyYTAzY2FkMGFkIiwiYWNyIjoiMSIsImFsbG93ZWQtb3JpZ2lucyI6WyIqIl0sInJlYWxtX2FjY2VzcyI6eyJyb2xlcyI6WyJvZmZsaW5lX2FjY2VzcyIsInVtYV9hdXRob3JpemF0aW9uIl19LCJyZXNvdXJjZV9hY2Nlc3MiOnsianVNUC1iYWNrZW5kIjp7InJvbGVzIjpbImFjZXNzb0FQSSJdfSwiTk9USUZJQ0FDQU8iOnsicm9sZXMiOlsiQWRtaW5pc3RyYWRvckNOSiIsIlRyaWJ1bmFsIl19LCJhY2NvdW50Ijp7InJvbGVzIjpbIm1hbmFnZS1hY2NvdW50IiwibWFuYWdlLWFjY291bnQtbGlua3MiLCJ2aWV3LXByb2ZpbGUiXX19LCJzY29wZSI6InByb2ZpbGUgZW1haWwiLCJlbWFpbF92ZXJpZmllZCI6dHJ1ZSwibmFtZSI6Ikl6YW4gQ2FsZGVyYXJvIiwicHJlZmVycmVkX3VzZXJuYW1lIjoiNjcwODQwMTAyOTciLCJnaXZlbl9uYW1lIjoiSXphbiIsImNvcnBvcmF0aXZvIjpbeyJzZXFfdXN1YXJpbyI6MTEyNjA2LCJub21fdXN1YXJpbyI6IklaQU4gRkFCUsOtQ0lPIE5FVkVTIENBTERFUkFSTyIsIm51bV9jcGYiOiI2NzA4NDAxMDI5NyIsInNlcV9vcmdhbyI6MSwic2lnX3RpcG9fY2FyZ28iOiJURVIiLCJmbGdfYXRpdm8iOiJOIiwic2VxX3Npc3RlbWEiOjExMywic2VxX3BlcmZpbCI6Mzk4LCJkc2Nfb3JnYW8iOiJDb25zZWxobyBOYWNpb25hbCBkZSBKdXN0acOnYSIsImRzY190cmlidW5hbF9wYWkiOiJDb25zZWxobyBOYWNpb25hbCBkZSBKdXN0acOnYSIsInNlcV90cmlidW5hbF9wYWkiOjEsImRzY19lbWFpbCI6Iml6YW4uY2FsZGVyYXJvQGNuai5qdXMuYnIiLCJkc2NfcGVyZmlsIjoiQWRtaW5pc3RyYWRvckNOSiIsImRzY19zaXN0ZW1hIjoiU2VydmnDp28gZGUgTm90aWZpY2HDp8O1ZXMiLCJkc2Nfc2lzdGVtYV9zaWdsYSI6Ik5PVElGSUNBQ0FPIn0seyJzZXFfdXN1YXJpbyI6MTEyNjA2LCJub21fdXN1YXJpbyI6IklaQU4gRkFCUsOtQ0lPIE5FVkVTIENBTERFUkFSTyIsIm51bV9jcGYiOiI2NzA4NDAxMDI5NyIsInNlcV9vcmdhbyI6MSwic2lnX3RpcG9fY2FyZ28iOiJURVIiLCJmbGdfYXRpdm8iOiJOIiwic2VxX3Npc3RlbWEiOjExMywic2VxX3BlcmZpbCI6Mzk5LCJkc2Nfb3JnYW8iOiJDb25zZWxobyBOYWNpb25hbCBkZSBKdXN0acOnYSIsImRzY190cmlidW5hbF9wYWkiOiJDb25zZWxobyBOYWNpb25hbCBkZSBKdXN0acOnYSIsInNlcV90cmlidW5hbF9wYWkiOjEsImRzY19lbWFpbCI6Iml6YW4uY2FsZGVyYXJvQGNuai5qdXMuYnIiLCJkc2NfcGVyZmlsIjoiVHJpYnVuYWwiLCJkc2Nfc2lzdGVtYSI6IlNlcnZpw6dvIGRlIE5vdGlmaWNhw6fDtWVzIiwiZHNjX3Npc3RlbWFfc2lnbGEiOiJOT1RJRklDQUNBTyJ9LHsic2VxX3VzdWFyaW8iOjExMjYwNiwibm9tX3VzdWFyaW8iOiJJWkFOIEZBQlLDrUNJTyBORVZFUyBDQUxERVJBUk8iLCJudW1fY3BmIjoiNjcwODQwMTAyOTciLCJzZXFfb3JnYW8iOjEyNzI4LCJzaWdfdGlwb19jYXJnbyI6IlRFUiIsImZsZ19hdGl2byI6Ik4iLCJzZXFfc2lzdGVtYSI6MTEzLCJzZXFfcGVyZmlsIjozOTksImRzY19vcmdhbyI6IlN1cGVyaW9yIFRyaWJ1bmFsIGRlIEp1c3Rpw6dhIiwiZHNjX3RyaWJ1bmFsX3BhaSI6IlN1cGVyaW9yIFRyaWJ1bmFsIGRlIEp1c3Rpw6dhIiwic2VxX3RyaWJ1bmFsX3BhaSI6MTI3MjgsImRzY19lbWFpbCI6Iml6YW4uY2FsZGVyYXJvQGNuai5qdXMuYnIiLCJkc2NfcGVyZmlsIjoiVHJpYnVuYWwiLCJkc2Nfc2lzdGVtYSI6IlNlcnZpw6dvIGRlIE5vdGlmaWNhw6fDtWVzIiwiZHNjX3Npc3RlbWFfc2lnbGEiOiJOT1RJRklDQUNBTyJ9LHsic2VxX3VzdWFyaW8iOjExMjYwNiwibm9tX3VzdWFyaW8iOiJJWkFOIEZBQlLDrUNJTyBORVZFUyBDQUxERVJBUk8iLCJudW1fY3BmIjoiNjcwODQwMTAyOTciLCJzZXFfb3JnYW8iOjEyOTkxLCJzaWdfdGlwb19jYXJnbyI6IlRFUiIsImZsZ19hdGl2byI6Ik4iLCJzZXFfc2lzdGVtYSI6MTEzLCJzZXFfcGVyZmlsIjozOTksImRzY19vcmdhbyI6IlRyaWJ1bmFsIFN1cGVyaW9yIEVsZWl0b3JhbCIsImRzY190cmlidW5hbF9wYWkiOiJUcmlidW5hbCBTdXBlcmlvciBFbGVpdG9yYWwiLCJzZXFfdHJpYnVuYWxfcGFpIjoxMjk5MSwiZHNjX2VtYWlsIjoiaXphbi5jYWxkZXJhcm9AY25qLmp1cy5iciIsImRzY19wZXJmaWwiOiJUcmlidW5hbCIsImRzY19zaXN0ZW1hIjoiU2VydmnDp28gZGUgTm90aWZpY2HDp8O1ZXMiLCJkc2Nfc2lzdGVtYV9zaWdsYSI6Ik5PVElGSUNBQ0FPIn0seyJzZXFfdXN1YXJpbyI6MTEyNjA2LCJub21fdXN1YXJpbyI6IklaQU4gRkFCUsOtQ0lPIE5FVkVTIENBTERFUkFSTyIsIm51bV9jcGYiOiI2NzA4NDAxMDI5NyIsInNlcV9vcmdhbyI6MTM2MDYsInNpZ190aXBvX2NhcmdvIjoiVEVSIiwiZmxnX2F0aXZvIjoiTiIsInNlcV9zaXN0ZW1hIjoxMTMsInNlcV9wZXJmaWwiOjM5OSwiZHNjX29yZ2FvIjoiVHJpYnVuYWlzIGRlIEp1c3Rpw6dhIEVzdGFkdWFpcyIsImRzY190cmlidW5hbF9wYWkiOiJUcmlidW5haXMgZGUgSnVzdGnDp2EgRXN0YWR1YWlzIiwic2VxX3RyaWJ1bmFsX3BhaSI6MTM2MDYsImRzY19lbWFpbCI6Iml6YW4uY2FsZGVyYXJvQGNuai5qdXMuYnIiLCJkc2NfcGVyZmlsIjoiVHJpYnVuYWwiLCJkc2Nfc2lzdGVtYSI6IlNlcnZpw6dvIGRlIE5vdGlmaWNhw6fDtWVzIiwiZHNjX3Npc3RlbWFfc2lnbGEiOiJOT1RJRklDQUNBTyJ9XSwiZmFtaWx5X25hbWUiOiJDYWxkZXJhcm8iLCJlbWFpbCI6Iml6YW4uY2FsZGVyYXJvQGNuai5qdXMuYnIifQ.Z_l8rWPzue3P0XAaYBcswQvYATFwzGFneAYTvbl0SI7_gH3JrOdweviQqd-IAsLttZhZ23XgiE4HvpBR-hYeSDOe7gPZpLlIOHwwRVzHgSooIbANlKDpyp9-qH8OGMSt0BXAfcBFsMtfCFGDsAakYI_QCaQUumAr43D9URWS6X8kIEwpAtpWobeu8EfG8JoMZt_uopZrx8neca9Pl_dprz_s8e3BNk2D80i109R0N05n5ZEeeesZNCiK6tJ6E7dhuOrbo0vbRwVIgKrwwXyrKlwtnvZfpc-x4uK8kTXSpPkM1hBg6jMOmyPWAvWDDpC7d8-i9ZmnlayH5inWnpdiyg';
+    const token = '';
 
-    return token
+    return token;
   }
-
 }
